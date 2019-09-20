b3dc8b4c34ea00273ef8afd81ce01aa97c9a3c0c
georgetheof <33423997+georgetheof@users.noreply.github.com>
2019-09-20 13:54:46 +0300

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,34 @@
+function MaximalSquare(strArr) {
+    if (strArr.every(x => { return (x.indexOf('1') === -1)})) return '0'; //check if '1' character exists at least once
+    let newArr = strArr.map(x => x.split('')); //format given array into a two dimensional array
+    let maxSquare = 1;
+
+    for (let i = 0, rows = newArr.length, cols = newArr[0].length; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (newArr[i][j] === '1') {
+                let returnedSquare = findMaxSquare(newArr, i, j, 1);
+                if (returnedSquare > maxSquare) maxSquare = returnedSquare;
+            }
+        }
+    }
+    
+    return maxSquare*maxSquare;
+}
+
+function findMaxSquare (firstArr, rows, cols, level) {
+    if ((rows + level < firstArr.length) && (cols + level < firstArr[0].length)) {  
+        let recurseMe = true;
+        for (let i = 0; i <= level; i++) {
+            if (!(firstArr[rows + level][cols + i] === '1' &&
+                  firstArr[rows + i][cols + level] === '1')) {   //will check the square of a given element
+                    recurseMe = false;                           //based on the level
+            }
+        }
+    
+        if (recurseMe) return findMaxSquare(firstArr, rows, cols, ++level); //if the first square consists of '1'
+    }                                                                       //function will recurse itself while increasing the level
+    else return level;                                                      //in order to check the next bigger
+}                                                                           //square from the given element
+
+// keep this function call here 
+MaximalSquare(readline());                            
